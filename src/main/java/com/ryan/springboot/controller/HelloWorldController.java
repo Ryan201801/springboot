e246878265c9e494ccf1c33672c13eaaa57784e5
Ryan201801 <815267338@qq.com>
2018-02-25 23:39:34 +0800

    看完了配置文件

--- src/main/java/com/ryan/springboot/controller/HelloWorldController.java
+++ src/main/java/com/ryan/springboot/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.ryan.springboot.controller;
 
+import com.ryan.springboot.domain.Girl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+    /*@Value("${cupSize}")
+    private String cupSize;*/
+    @Autowired
+    private Girl girl;
+
     @RequestMapping("/hello")
     public String index() {
-        return "Hello World";
+        return girl.getAge().toString();
     }
 }
